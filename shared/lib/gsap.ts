893d84d92e46e2bd0e374ebaabe4a022e5bc9968
Farid Azhari <faridazhari46@gmail.com>
2026-05-03 04:15:57 +0700

    perf(bundle): remove 6 unused GSAP plugins to reduce JS bundle size

--- shared/lib/gsap.ts
+++ shared/lib/gsap.ts
@@ -2,37 +2,20 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import {
   ScrollTrigger,
-  Flip,
-  MotionPathPlugin,
-  DrawSVGPlugin,
-  PixiPlugin,
-  Draggable,
   ScrollToPlugin,
   SplitText,
-  MorphSVGPlugin,
 } from "gsap/all";
 
 gsap.registerPlugin(
   ScrollTrigger,
-  Flip,
-  MotionPathPlugin,
-  DrawSVGPlugin,
-  PixiPlugin,
-  Draggable,
   ScrollToPlugin,
   SplitText,
-  MorphSVGPlugin,
 );
 
 export {
   useGSAP,
   ScrollTrigger,
-  Flip,
-  MotionPathPlugin,
-  DrawSVGPlugin,
-  Draggable,
   ScrollToPlugin,
   SplitText,
-  MorphSVGPlugin,
 };
 export default gsap;
